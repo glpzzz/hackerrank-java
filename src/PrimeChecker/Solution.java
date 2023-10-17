6c5045f56a1c749bbb6db50c2fb20b2728297774
Gabriel A. López López <glpz@daxslab.com>
2023-10-17 16:10:11 -0400

    Add solution for PrimeChecker.

---
+++ src/PrimeChecker/Solution.java
@@ -0,0 +1,88 @@
+package PrimeChecker;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+class Prime {
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        if (number <= 3) {
+            return true;
+        }
+
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void checkPrime(int... numbers) {
+        String output = "";
+
+        for (int i = 0; i < numbers.length; i++){
+            if(isPrime(numbers[i])){
+                output += numbers[i]+" ";
+            }
+        }
+
+        System.out.println(output.trim());
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            int n1=Integer.parseInt(br.readLine());
+            int n2=Integer.parseInt(br.readLine());
+            int n3=Integer.parseInt(br.readLine());
+            int n4=Integer.parseInt(br.readLine());
+            int n5=Integer.parseInt(br.readLine());
+            Prime ob=new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1,n2);
+            ob.checkPrime(n1,n2,n3);
+            ob.checkPrime(n1,n2,n3,n4,n5);
+            Method[] methods=Prime.class.getDeclaredMethods();
+            Set<String> set=new HashSet<>();
+            boolean overload=false;
+            for(int i=0;i<methods.length;i++)
+            {
+                if(set.contains(methods[i].getName()))
+                {
+                    overload=true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if(overload)
+            {
+                throw new Exception("Overloading not allowed");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
+
