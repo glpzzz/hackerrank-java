8e3d746d9cea87e55afac6a0ea60328415a28f83
Gabriel A. López López <glpz@daxslab.com>
2023-10-18 12:57:35 -0400

    Add solution for JavaExceptionHandlingTryCatch.

---
+++ src/JavaExceptionHandlingTryCatch/Solution.java
@@ -0,0 +1,20 @@
+package JavaExceptionHandlingTryCatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try{
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x/y);
+        }catch(InputMismatchException e){
+            System.out.println(e.getClass().getName());
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
