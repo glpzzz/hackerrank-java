379dbade9b25027cf4967a82aa6f07e2ee40d9c8
Gabriel A. López López <glpz@daxslab.com>
2023-10-18 00:47:12 -0400

    Add solution for JavaCurrencyFormatter.

---
+++ src/JavaCurrencyFormatter/Solution.java
@@ -0,0 +1,25 @@
+package JavaCurrencyFormatter;
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        Locale in = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(in).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
